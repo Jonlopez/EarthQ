567c4aca6852705f92d4c46b8f4e7161546998e6
Unknown <jonlopezgarrido@gmail.com>
2017-01-17 21:18:29 +0100

    Creación de proyecto con podfiles JSON

---
+++ EarthQ/EarthQ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EarthQ
+//
+//  Created by  on 17/1/17.
+//  Copyright © 2017 8fprogmm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
