0f4b54fe3ea2ae87d035a03d262e6342f1df1a61
Unknown <jonlopezgarrido@gmail.com>
2017-02-07 22:03:14 +0100

    avances varios

---
+++ EarthQ/EarthQ/PrincipalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PrincipalTableViewCell.swift
+//  EarthQ
+//
+//  Created by  on 7/2/17.
+//  Copyright © 2017 8fprogmm. All rights reserved.
+//
+
+import UIKit
+
+class PrincipalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
